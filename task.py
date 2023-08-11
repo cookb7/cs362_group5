b0bc3b87b01312ebecb405190cff5c4cb7278dfb
cookb7 <cookb7@oregonstate.edu>
2023-08-11 13:22:55 -0600

    Wrote basic function for my_datetime and wrote simple tests for my_datetime

--- task.py
+++ task.py
@@ -3,7 +3,24 @@ def conv_num(num_str):
 
 
 def my_datetime(num_sec):
-    pass
+    """Defines a function that takes a number of seconds and
+    returns the date after 01-01-1970."""
+    SECONDS_IN_DAY = 86400
+
+    days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    num_days = num_sec // SECONDS_IN_DAY
+    year = 1970
+
+    month = 1
+    while num_days >= days_in_month[month - 1]:
+        num_days -= days_in_month[month - 1]
+        month += 1
+
+    day = num_days + 1 # Add 1 to get correct date format
+    date = f"{month:02d}-{day:02d}-{year}"
+
+    return date
 
 
 def conv_endian(num, endian='big'):
--- tests.py
+++ tests.py
@@ -1,10 +1,28 @@
 import unittest
+import task
 
 
 class TestCase(unittest.TestCase):
 
     def test1(self):
-        self.assertTrue(True)
+        """Checks my_datetime for a base case of 0."""
+        expected = '01-01-1970'
+        self.assertEqual(task.my_datetime(0), expected,\
+            "A value of 0 does not return 01-01-1970.")
+
+    def test2(self):
+        """Checks my_datetime that one day in seconds returns 01-02-1970."""
+        expected = '01-02-1970'
+        num_sec = 86400
+        self.assertEqual(task.my_datetime(num_sec), expected,\
+            msg=" A value of 86400 seconds or 1 day does not return 01-02-1970.")
+
+    def test3(self):
+        """Checks my_datetime with a value of one month."""
+        expected = '02-01-1970'
+        num_sec = 2678400
+        self.assertEqual(task.my_datetime(num_sec), expected,\
+            msg="A value of 2678400 or one month does not return 02-01-1970")
 
 
 if __name__ == '__main__':
